5ca007d92d67b7fffde0fdc9966646569ed21c8d
Dorin <dorinhazan052@gmail.com>
2019-09-14 17:29:30 +0300

    make it better

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const path = require( 'path' )
 const youtube = require( './app/lib/youtube' )
 
 const SongsApi = require('./app/routes/songs-router')
-// const xyz = require('./app/lib/SongsApi')
 const spotify = require('./app/lib/spotify')
 const lyrics = require('./app/lib/lyrics')
 const PORT = 3031
@@ -19,7 +18,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/', SongsApi)
-// app.use('/', xyz)
 // app.use('/wishlist', wishlistApi)
 
 mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost/music-App", { useNewUrlParser: true }).then( () => {
